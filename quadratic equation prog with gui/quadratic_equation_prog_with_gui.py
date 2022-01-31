a56d7e79ae10cd304dec6d4b327f3143d6500815
Infoman724 <93324312+Infoman724@users.noreply.github.com>
2022-01-31 17:36:38 +0200

    process

--- quadratic equation prog with gui/quadratic_equation_prog_with_gui.py
+++ quadratic equation prog with gui/quadratic_equation_prog_with_gui.py
@@ -1,21 +1,53 @@
 from tkinter import *
-def SQUARE(event):
-    a=float(Z1.get())
-    b=float(Z2.get())
-    c=float(Z3.get())
-    D=b**2-4*a*c
-    vast.configure(text=D)
-    if D<0:
-        vast.configure(text=f"корней нет={D}")
-        
-    elif D>0:
-        k1=(-b+D**0.5)/(2*a)
-        k2=(-b-D**0.5)/(2*a)
-        vast.configure(text=f"D={D}\n K1={k1}\n K2={k2}")
+from math import *
 
-    elif D==0:
-        x=-b/2*a
-        vast.configure(text=f"один корень={D}")
+def SQUARE():
+    if (a.get()!="" and b.get()!="" and c.get()!=""):
+        a=float(Z1.get())
+        b=float(Z2.get())
+        c=float(Z3.get())
+        D=b**2-4*a*c
+        vast.configure(text=D)
+        
+        if D>0:
+            k1=round((-b+D**0.5)/(2*a),2)
+            k2=round((-b-D**0.5)/(2*a),2)
+            geg=f"K1={k1} \n K2={k2}"
+            flag=True
+            vast.configure(text=f"D={D}\n K1={k1}\n K2={k2}")
+
+        elif D==0:
+            x=-b/2*a
+            geg=f"X={x}"
+            flag=True
+            vast.configure(text=f"один корень={D}")
+
+        else:
+            vast.configure(text=f"корней нет={D}")
+            flag=False
+        return flag,D,geg
+
+def graafik():
+    flag,D,geg=SQUARE()
+    if flag==True:
+        a1=int(a.get())
+        b1=int(b.get())
+        c1=int(c.get())
+        x0=(-b1)/(2*a1)
+        y0=a1*x0*x0+b1*x0+c1
+        x = np.arange(x0-10, x0+10, 0.5)
+        y=a1*x*x+b1*x+c1
+        fig = plt.figure()
+        plt.plot(x, y,'b:o', x0, y0,'g-d')
+        plt.title('Квадратное уравнение')
+        plt.ylabel('y')
+        plt.xlabel('x')
+        plt.grid(True)
+        plt.show()
+        text=f"Вершина параболлы ({x0},{y0})"
+    else:
+        text=f"График нет возможности построить"
+    answer.configure(text=f"D={D}\n{t}\n{text}")
 
 
 
@@ -33,8 +65,10 @@ text1=Label(aken,text="x**2+",height=1,width=4,font="Arial 20",fg="green",bg="wh
 text2=Label(aken,text="x+",height=1,width=2,font="Arial 20",fg="green",bg="white")
 text3=Label(aken,text="=0",height=1,width=2,font="Arial 20",fg="green",bg="white")
 
-BT=Button(aken,text="Решение",font="Arial 20",height=1,width=8,bg="green",fg="black",)
-BT.bind("<Button-1>",SQUARE)
+BT=Button(aken,text="Решение",font="Arial 20",height=1,width=8,bg="green",fg="black",command=SQUARE)
+
+btn_g=Button(aken,text="График",font="Calibri 26",bg="white",command=graafik)
+btn_g.pack(side=LEFT)
 
 
 Z1=Entry(aken,width=4,font="Arial 20",fg="blue",bg="lightblue", justify=CENTER)
