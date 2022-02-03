cc920c405a36562fd1b7098f70e8c31ecc2d80c8
Infoman724 <93324312+Infoman724@users.noreply.github.com>
2022-02-03 17:29:02 +0200

    Finish with new graff

--- quadratic equation prog with gui/quadratic_equation_prog_with_gui.py
+++ quadratic equation prog with gui/quadratic_equation_prog_with_gui.py
@@ -2,6 +2,10 @@ from tkinter import *
 from math import *
 import matplotlib.pyplot as plt
 import numpy as np
+global D,t
+D=-1
+t=""
+T=0
 
 def SQUARE():
     """
@@ -57,13 +61,153 @@ def graafik():
     else:
         text=f"График нет возможности построить"
     vast.configure(text=f"D={D}\n{geg}\n{text}")
+
+def veel() :
+    global T 
+    if T==0:
+        aken.geometry(str(aken.winfo_width())+"x"+str(aken.winfo_width()+50))
+        btn_veel.config(text="Уменьшить окно")
+        T=1
+    else:
+        aken.geometry(str(aken.winfo_width())+"x"+str(aken.winfo_width()-500))
+        btn_veel.config(text="Увеличеть окно")
+        T=0
+
+def figura():
+    valik=var.get()
+    if valik==1:
+        glass()
+    elif valik==2:
+        whale()
+    elif valik==3:
+        zont()
+    elif valik==4:
+        babochka()
+
+def glass():
+        x1 = np.arange(-9, -0.5 , 0.5)#min max step
+        y1=(-1/16)*(x1+5)**2 +2       
+        x2 = np.arange(1, 9.5, 0.5)
+        y2=(-1/16)*(x2-5)**2 +2        
+        x3 = np.arange(-9, -0.5 , 0.5)
+        y3= (1/4)*(x3+5)**2 -3
+        x4 = np.arange(1, 9.5 , 0.5)
+        y4= (1/4)* (x4-5)** 2 -3
+        x5 = np.arange(-9, -5.5 , 0.5)
+        y5= у = -(x5+7)** 2 +5
+        x6 = np.arange(6, 9.5 , 0.5)
+        y6= -(x6-7)** 2 +5
+        x7 = np.arange(-1, 1.5, 0.5)
+        y7= -0.5*x7**2+1.5
+
+        plt.plot(x1, y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7)
+        plt.title('очки')
+        plt.ylabel('y')
+        plt.xlabel('x')
+        plt.grid(True)
+        plt.show()
+
+def whale():
+    x1 = np.arange(0, 9.5, 0.5)#min max step
+    y1=(2/27)*x1*x1-3
+    x2 = np.arange(-10, 0.5, 0.5)#min max step
+    y2=0.04*x2*x2-3
+    x3 = np.arange(-9, -2.5, 0.5)#min max step
+    y3=(2/9)*(x3+6)**2+1
+    x4 = np.arange(-3, 9.5, 0.5)#min max step
+    y4=(-1/12)*(x4-3)**2+6
+    x5 = np.arange(5, 9, 0.5)#min max step
+    y5=(1/9)*(x5-5)**2+2
+    x6 = np.arange(5, 8.5, 0.5)#min max step
+    y6=(1/8)*(x6-7)**2+1.5
+    x7 = np.arange(-13, -8.5, 0.5)#min max step
+    y7=(-0.75)*(x7+11)**2+6
+    x8 = np.arange(-15, -12.5, 0.5)#min max step
+    y8=(-0.5)*(x8+13)**2+3
+    x9 = np.arange(-15, -10, 0.5)#min max step
+    y9=[1]*len(x9)
+    x10 = np.arange(3, 4, 0.5)#min max step
+    y10=[3]*len(x10)
+    fig = plt.figure()
+    plt.plot(x1, y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10)
+    plt.title('Кит')
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
+
+def zont():
+    x1=np.arange(-12, 12.5, 0.5)
+    y1=(-1/18)*x1**2+12
+    x2=np.arange(-4, 4.5, 0.5)
+    y2=(-1/8)*x2**2+6
+    x3=np.arange(-12, -3.5, 0.5)
+    y3=(-1/8)*(x3+8)**2+6
+    x4=np.arange(4, 12.5, 0.5)
+    y4=(-1/8)*(x4-8)**2+6
+    x5=np.arange(-4, -0.3, 0.5)
+    y5=2*(x5+3)**2-9
+    x6=np.arange(-4, 0.2, 0.5)
+    y6=1.5*(x6+3)**2-10
+    fig=plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6)
+    plt.title("Зонт")
+    plt.ylabel("y")
+    plt.xlabel("x")
+    plt.grid(True)
+    plt.show()
+
+def babochka():
+    x1 = np.arange(-9, -0.5, 0.5)
+    y1 = -(1/8)*(x1+9)**2+8
+    x2 = np.arange(1, 9.5, 0.5)
+    y2 = -(1/8)*(x2-9)**2+8
+    x3 = np.arange(-9, -7.5, 0.5)
+    y3 = 7*(x3+8)**2+1
+    x4 = np.arange(8, 9.5, 0.5)
+    y4 = 7*(x4-8)**2+1
+    x5 = np.arange(-8, -0.5, 0.5)
+    y5 = 1/49*(x5+1)**2
+    x6 = np.arange(1, 8.5, 0.5)
+    y6=1/49*(x6-1)**2
+    x7 = np.arange(-8, -0.5, 0.5)
+    y7 = -4/49*(x7+1)**2
+    x8 = np.arange(1, 8.5, 0.5)
+    y8 = -4/49*(x8-1)**2
+    x9 = np.arange(-8, -1.5, 0.5)
+    y9 = 1/3*(x9+5)**2-7
+    x10 = np.arange(2, 8.5, 0.5)
+    y10 = 1/3*(x10-5)**2-7
+    x11 = np.arange(-2, -0.5, 0.5)
+    y11 = -2*(x11+1)**2-2
+    x12 = np.arange(1, 2.5, 0.5)
+    y12 = -2*(x12-1)**2-2
+    x13 = np.arange(-1, 1.5, 0.5)
+    y13 = -4*x13**2+2
+    x14 = np.arange(-1, 1.5, 0.5)
+    y14 = 4*x14**2-6
+    x15 = np.arange(-2, 0.5, 0.5)
+    y15 = -1.5*x15+2
+    x16 = np.arange(0, 2.5, 0.5)
+    y16 = 1.5*x16+2
+    fig = plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10,x11,y11,x12,y12,x13,y13,x14,y14,x15,y15,x16,y16)
+    plt.title('Бабочка')
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
 #-------------------------------------------------------------------------------------------------------------
 
 
 #каркас интерфейса
 aken=Tk()
 aken.title("Square equation with gui")
-aken.geometry("650x300")
+aken.geometry("900x375")
+f1=Frame(aken,width=900,height=300,bg="blue")
+f1.place(x=0,y=0)
+f2=Frame(aken,width=900,height=600,bg="yellow")
+f2.place(x=0,y=300)
 #---------------------------------------------------------------------------------------------------------------------------------
 #лейблы
 lbl=Label(aken,text="Решение квадратного уравнения ",height=2,width=37,font="Arial 15",fg="green",bg="lightblue")
@@ -79,8 +223,14 @@ text3=Label(aken,text="=0",height=1,width=2,font="Arial 20",fg="green",bg="white
 BT=Button(aken,text="Решение",font="Arial 20",height=1,width=8,bg="green",fg="black",command=SQUARE)
 
 btn_g=Button(aken,text="График",font="Calibri 26",bg="green",fg="black",command=graafik)
-btn_g.pack(side=RIGHT)
 
+btn_veel=Button(f2,text="Увеличеть окно", font="Calibri 26", bg="green",command=veel)
+
+var=IntVar()
+r1=Radiobutton(f2,text="очки",variable=var,value=1,font="Calibri 26",command=figura)#command=figura
+r2=Radiobutton(f2,text="кит",variable=var,value=2,font="Calibri 26",command=figura)
+r3=Radiobutton(f2,text="зонтик",variable=var,value=3,font="Calibri 26",command=figura)
+r3=Radiobutton(f2,text="бабочка",variable=var,value=4,font="Calibri 26",command=figura)
 
 a=Entry(aken,width=4,font="Arial 20",fg="blue",bg="lightblue", justify=CENTER)
 b=Entry(aken,width=4,font="Arial 20",fg="blue",bg="lightblue", justify=CENTER)
@@ -98,7 +248,13 @@ text2.place(x=215,y=100)
 text3.place(x=320,y=100)
 
 vast.place(x=70,y=150)
+
 BT.place(x=370,y=90)
+btn_veel.place(x=260,y=0)
+r1.place(x=200,y=200)
+r2.place(x=100,y=300)
+r3.place(x=300,y=100)
+btn_g.place(x=550,y=80)
 
 
 
